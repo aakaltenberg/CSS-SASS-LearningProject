42d0715bf86a5ba72ce6dc0ac2027e9b89a16b97
kaltenberg <a.a.kaltenberg@yandex.ru>
2022-04-12 11:55:03 +0300

    adding css project

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const start = document.querySelector('#start'),
+progress = document.querySelector('#progress'),
+preview = document.querySelector('.preview');
+
+// start.addEventListener('click', work);
